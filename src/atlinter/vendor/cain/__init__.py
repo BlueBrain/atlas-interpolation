498ccdd8b72f3c377d90c3bfdf5629d25a0329d3
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2021-10-25 13:13:22 +0200

    Add license headers to the CAIN vendor

---
+++ src/atlinter/vendor/cain/__init__.py
@@ -0,0 +1,2 @@
+# SPDX-License-Identifier: MIT
+# Source: https://github.com/myungsub/CAIN/tree/fff8fc321c5a76904ed2a12c9500e055d4c77256
--- src/atlinter/vendor/cain/cain.py
+++ src/atlinter/vendor/cain/cain.py
@@ -1,3 +1,5 @@
+# SPDX-License-Identifier: MIT
+# Source: https://github.com/myungsub/CAIN/tree/fff8fc321c5a76904ed2a12c9500e055d4c77256
 import math
 import numpy as np
 
